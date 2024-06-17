d7c455bee597646356f993fe7edf68650b3df350
vivek1729 <1658576+vivek1729@users.noreply.github.com>
2024-06-17 15:24:28 -0700

    Add repro for typing issue with apache-arrow import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// import { tableFromArrays } from 'apache-arrow';
+// function createArrowTable() {
+//   // Create a Table from JavaScript arrays sample from https://arrow.apache.org/docs/js/
+//   const LENGTH = 2000;
+//     const rainAmounts = Float32Array.from(
+//         { length: LENGTH },
+//         () => Number((Math.random() * 20).toFixed(1)));
+//     const rainDates = Array.from(
+//         { length: LENGTH },
+//         (_, i) => new Date(Date.now() - 1000 * 60 * 60 * 24 * i));
+//     const rainfall = tableFromArrays({
+//         precipitation: rainAmounts,
+//         date: rainDates
+//     });
+//     console.table([...rainfall]);
+// }
 
 @Component({
   selector: 'app-root',
@@ -8,6 +25,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'apache-arrow-angular-type-issue-repro';
+export class AppComponent implements OnInit {
+  title = 'my-app';
+
+  ngOnInit() {
+    const timer: number = setTimeout(() => {
+      // createArrowTable();
+      console.log('This will be logged after 2 seconds');
+    }, 2000);
+    console.log(timer);
+  }
 }
